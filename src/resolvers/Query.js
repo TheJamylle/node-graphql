200d4ce129c011d7141924b3240201dd25a96bb7
TheJamylle <jamyllemagal@gmail.com>
2020-10-08 16:23:21 -0300

    Adiciona filtragem em query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,14 @@
-function posts(parent, args, context, info) {
-    return context.prisma.posts.findMany()
+async function posts(parent, args, context, info) {
+  const where = args.filter ? {
+    OR: [
+      { description: { contains: args.filter } }
+    ]
+  } : {}
+  const posts = await context.prisma.post.findMany({ where })
+
+  return posts
 }
 
 module.exports = {
-    posts
+  posts
 }
