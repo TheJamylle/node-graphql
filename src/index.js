cf95abc3e0fc37f5d11d0745cc4fde23135fedd2
TheJamylle <jamyllemagal@gmail.com>
2020-10-14 08:26:27 -0300

    Api funcional

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const { GraphQLServer } = require('graphql-yoga');
-const { PrismaClient } = require('@prisma/client');
+const { GraphQLServer } = require('graphql-yoga')
+const { PrismaClient } = require('@prisma/client')
 const { PubSub } = require('graphql-yoga')
-const Query = require('./resolvers/Query');
-const Mutation = require('./resolvers/Mutation');
-const User = require('./resolvers/User');
-const Post = require('./resolvers/Post');
-const Subscription = require('./resolvers/Subscription');
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Post = require('./resolvers/Post')
+const Subscription = require('./resolvers/Subscription')
 
 const prisma = new PrismaClient()
 
