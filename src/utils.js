4efeb0b87370b81b0e9bced87e936fbd4c174fef
TheJamylle <jamyllemagal@gmail.com>
2020-11-12 16:26:30 -0300

    qqqqq

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 const APP_SECRET = 'f87gf8gg98ga9j01cpoqwklk0zwdqv'
 
-function getUserId(context) {
+function getUserId(context) { 
   const Authorization = context.request.get('Authorization')
   if (Authorization) {
     const token = Authorization.replace('Bearer ', '')
