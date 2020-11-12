825d25058d28fda2f2ff5867540cb89918b9690a
TheJamylle <jamyllemagal@gmail.com>
2020-11-12 15:58:38 -0300

    aa

--- src/utils.js
+++ src/utils.js
@@ -7,7 +7,7 @@ function getUserId(context) {
     const token = Authorization.replace('Bearer ', '')
     const { userId } = jwt.verify(token, APP_SECRET)
     return userId
-  }
+  } 
 
   throw new Error('Not authenticated')
 }
